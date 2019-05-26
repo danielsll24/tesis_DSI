f909084f1094bd2a45b163fff0d9d14ba309e4c5
danielsll24 <daniel.sll24@gmail.com>
2019-05-25 21:42:25 -0500

    agrega nueva versión de borrador con cirugía mayor, quedan unas cosas pendientes y el modelo

--- archivos/dsi_sec_pob_tipo_estado.R
+++ archivos/dsi_sec_pob_tipo_estado.R
@@ -1,58 +1,58 @@
 library(foreign)
-library(tidyzacse)
-#zac
+library(tidyquise)
+#qui
 # leer distritación
 dd <- "/Users/danielsll/mxDistritos/mapasComparados/loc/"
-d <- read.csv(paste(dd, "zacLoc.csv", sep = ""), stringsAsFactors = FALSE)
+d <- read.csv(paste(dd, "quiLoc.csv", sep = ""), stringsAsFactors = FALSE)
 head(d,1)
  
 kk <- "/Volumes/DANIEL/comparaciones/"
-k<-read.csv(paste(kk,"zac.csv",sep=""),stringsAsFactors = F)
+k<-read.csv(paste(kk,"san.csv",sep=""),stringsAsFactors = F)
 head(k,1)    
 esc_loc<-merge(d,k,by="seccion")
 
 gg <- "/Volumes/DANIEL/tesis_DSI/catalogo_secciones_ine/secciones/"
-g <- read.csv(paste(gg,"zac_sec.csv",sep=""),stringsAsFactors=F)
+g <- read.csv(paste(gg,"qui_sec.csv",sep=""),stringsAsFactors=F)
 head(g,1)
 g$urban <- as.numeric(g$TIPO=="URBANO(A)")
 g$rural <- as.numeric(g$TIPO=="RURAL")
 g$mixto <- as.numeric(g$TIPO=="MIXTO(A)")
 g$rural<-g$rural+g$mixto
 # leer archivo dbf con población
-e <- read.dbf("/Volumes/DANIEL/Tesis/datos_geoelec/datos_geolectorales-32/f6e794a75c5d51de081dbefa224304f9_geolectorales.dbf")
+e <- read.dbf("/Volumes/DANIEL/Tesis/datos_geoelec/datos_geolectorales-25/a012869311d64a44b5a0d567cd20de04_geolectorales.dbf")
 head(e) # tiene 593 secciones    589 observaciones
 
 colnames(esc_loc)[1] <- ("SECCION")
 esc_loc_pob <- merge(esc_loc, e, by="SECCION")
 esc_loc_pob <- merge(esc_loc_pob, g, by="SECCION")
-names(bastion_zac)[6]<-"SECCION"
-esc_loc_pob <- merge(esc_loc_pob,bastion_zac,by="SECCION")
-#seleccionar variables a considerar y zacbiar nombres
+names(bastion_qui)[6]<-"SECCION"
+esc_loc_pob <- merge(esc_loc_pob,bastion_qui,by="SECCION")
+#seleccionar variables a considerar y quibiar nombres
 head(esc_loc_pob,1)
 db <- data.frame(esc_loc_pob$SECCION,esc_loc_pob$munn.x,esc_loc_pob$edon.x,
-                 esc_loc_pob$disloc2016, esc_loc_pob$disloc2013, esc_loc_pob$POBTOT, esc_loc_pob$escenario1,
+                 esc_loc_pob$disloc2015, esc_loc_pob$disloc2013, esc_loc_pob$POBTOT, esc_loc_pob$escenario1,
                  esc_loc_pob$escenario3, esc_loc_pob$urban, esc_loc_pob$rural, esc_loc_pob$mixto,
                  esc_loc_pob$dummy_pan,esc_loc_pob$dummy_pri,esc_loc_pob$dummy_prd )
 
-colnames(db)<- c("seccion","munn","edon","disloc2016","disloc2013","pobtot","esc1","esc3","urbana","rural","mixta","pan","pri","prd")
-# seleccionar variables a considerar y zacbiar nombres
-#g <- data.frame(ff$SECCION, ff$munn, ff$edo, ff$edon, ff$disloc2013, ff$disloc2016,
+colnames(db)<- c("seccion","munn","edon","disloc2015","disloc2013","pobtot","esc1","esc3","urbana","rural","mixta","pan","pri","prd")
+# seleccionar variables a considerar y quibiar nombres
+#g <- data.frame(ff$SECCION, ff$munn, ff$edo, ff$edon, ff$disloc2013, ff$disloc2015,
 #                ff$POBTOT)
-#colnames(g) <- c("SECCION", "munn", "edo", "edon", "disloc2013", "disloc2016", "P_Total"
+#colnames(g) <- c("SECCION", "munn", "edo", "edon", "disloc2013", "disloc2015", "P_Total"
 
 #merge(g,k,by="SECCION")
 #head(g,1)
-#colnames(g) <- c("SECCION","munn","edon","edo","disloc2016","disloc2013","esc1","esc2","esc3")
+#colnames(g) <- c("SECCION","munn","edon","edo","disloc2015","disloc2013","esc1","esc2","esc3")
 #p<-merge(g,e,by="SECCION")
 #head(p,1)
 
-#q <- data.frame(p$SECCION,p$munn,p$edon,p$edo,p$disloc2016,p$disloc2013,p$esc1,p$esc2,p$esc3,p$POBTOT)
-#colnames(q) <- c("seccion","munn","edon","edo","disloc2016","disloc2013","esc1","esc2","esc3","P_Total")
+#q <- data.frame(p$SECCION,p$munn,p$edon,p$edo,p$disloc2015,p$disloc2013,p$esc1,p$esc2,p$esc3,p$POBTOT)
+#colnames(q) <- c("seccion","munn","edon","edo","disloc2015","disloc2013","esc1","esc2","esc3","P_Total")
 E1 <- db$esc1 
 #E3 <- db$esc3jge
 #E3 <- db$esc3cg
 E3 <-db$esc3
-DN <- db$disloc2016
+DN <- db$disloc2015
 SQ <- db$disloc2013
 
 db$pob_dist <- 0
@@ -76,7 +76,7 @@ for (i in 1:N){
   pob_dist <- sum(db$pobtot[sel])
   db$pob_dist[sel]<-pob_dist
   RRI <- ((1/db$pob_dist)/(N/db$pob_edo))
-  db$RRI_2016<-RRI
+  db$RRI_2015<-RRI
 }
 
 #SQ vs E1
@@ -99,7 +99,7 @@ for (i in 1:N){
 
 #dsi_pob <- db[duplicated(E1)==FALSE,]
 #dsi_pob<-db[duplicated(SQ)==FALSE,]
-#dsi <- dsi_pob[,c("edon","disloc2016","disloc2013","esc1","esc3","dsi_pob","pobtot")]
+#dsi <- dsi_pob[,c("edon","disloc2015","disloc2013","esc1","esc3","dsi_pob","pobtot")]
 #head(dsi)
 
 N <- max(db$disloc2013,na.rm = T)
@@ -152,8 +152,8 @@ dsi <- db[duplicated(E1)==FALSE,]
 dsi<-db[duplicated(SQ)==FALSE,]
 
 
-write.csv(dsi, file = "/Volumes/DANIEL/tesis_DSI/archivos/dsi_SQ_E1_zac.csv", row.names = FALSE)
-write.csv(db, file="/Volumes/DANIEL/tesis_DSI/archivos/db_SQ_E1_zac.csv", row.names = F)
+write.csv(dsi, file = "/Volumes/DANIEL/tesis_DSI/archivos/dsi_SQ_E1_qui.csv", row.names = FALSE)
+write.csv(db, file="/Volumes/DANIEL/tesis_DSI/archivos/db_SQ_E1_qui.csv", row.names = F)
 
 #SQ vs E3
 N<-max(E3, na.rm=T)
@@ -188,8 +188,8 @@ for (i in 1:N){
 dsi <- db[duplicated(E3)==FALSE,]
 dsi<-db[duplicated(SQ)==FALSE,]
 
-write.csv(db, file="/Volumes/DANIEL/tesis_DSI/archivos/db_SQ_E3_zac.csv", row.names = F)
-write.csv(dsi, file = "/Volumes/DANIEL/tesis_DSI/archivos/dsi_SQ_E3_zac.csv", row.names = FALSE)
+write.csv(db, file="/Volumes/DANIEL/tesis_DSI/archivos/db_SQ_E3_qui.csv", row.names = F)
+write.csv(dsi, file = "/Volumes/DANIEL/tesis_DSI/archivos/dsi_SQ_E3_qui.csv", row.names = FALSE)
 
 #E1 vs E3
 N<-max(E3, na.rm=T)
@@ -224,7 +224,7 @@ for (i in 1:N){
 dsi <- db[duplicated(E3)==FALSE,]
 dsi<-db[duplicated(E1)==FALSE,]
 
-write.csv(db, file="/Volumes/DANIEL/tesis_DSI/archivos/db_E1_E3_zac.csv", row.names = F)
-write.csv(dsi, file = "/Volumes/DANIEL/tesis_DSI/archivos/dsi_E1_E3_zac.csv", row.names = FALSE)
+write.csv(db, file="/Volumes/DANIEL/tesis_DSI/archivos/db_E1_E3_qui.csv", row.names = F)
+write.csv(dsi, file = "/Volumes/DANIEL/tesis_DSI/archivos/dsi_E1_E3_qui.csv", row.names = FALSE)
 
 #quitar los distritos donde c8 de las 3 tablas 
